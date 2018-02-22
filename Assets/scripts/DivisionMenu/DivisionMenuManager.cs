51e451a751582ef24b6a5828be6a9d0c12062373
ryan-chomistek <ryan.chomistek@tamu.edu>
2018-02-22 12:41:24 -0600

    123

--- Assets/scripts/DivisionMenu/DivisionMenuManager.cs
+++ Assets/scripts/DivisionMenu/DivisionMenuManager.cs
@@ -3,11 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DivisionMenuManager : MonoBehaviour {
+    public GameObject backButton;
+    public GameObject divisionMenuItem;
+    public DivisionController commander;
+    List<DivisionController> subordinates;
+    List<DivisionMenuItem> displayedItems;
+    void Update()
+    {
+        if(subordinates.Count != commander.subordinates.Count)
+        {
+            subordinates = commander.subordinates;
+        }
+    }
+
+    void RefreshMenuItems()
+    {
+
+    }
+
+    void GoBack()
+    {
+        //go the the persons commander
+        //disable back button if he doesent have a commander
+    }
 
     public void DivisionMenuItemClicked(RememberedDivision division)
     {
         //bing up context menu for that division on the bottom right, but do different actions
         //need to send messengers to discover its location
-        //also bring up another window showing its sub command
+        //refresh the division menu with his stuff
     }
 }
